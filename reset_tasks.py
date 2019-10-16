3f224bcb0ae1d192971d75450e4c9846ed4391fc
zeyyla <anavmittal@berkeley.edu>
2019-10-16 00:38:45 -0700

    delete old tasks and results

--- reset_tasks.py
+++ reset_tasks.py
@@ -1,6 +1,19 @@
 from itertools import product, combinations, permutations
 from Task import Task, SPECIES, SUBFAMILIES
-import json 
+import pickle
+import json
+import shutil
+from os import mkdir
+try:
+	shutil.rmtree("tasks")
+except FileNotFoundError:
+	pass
+try:
+	shutil.rmtree("results")
+except FileNotFoundError:
+	pass
+mkdir("results")
+mkdir("tasks")
 
 tasks = list(product(permutations(SPECIES, 2), SUBFAMILIES))
 data_structure = json.load(open("data/data_structure.json", "r"))
@@ -8,5 +21,5 @@ for task in tasks:
 	species1, species2, subfamily = task[0][0], task[0][1], task[1]
 	species1_data = data_structure[species1]['names'].index(species1 + "_" + subfamily + ".p")
 	total = data_structure[species1]['lens'][species1_data]
-    t = Task(task[0][0], task[0][1], task[1], total)
-    pickle.dump(t, open("tasks/"+t.filename(), "wb+"))
+	t = Task(task[0][0], task[0][1], task[1], total)
+	pickle.dump(t, open("tasks/"+t.filename(), "wb+"))
