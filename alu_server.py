bd861daaaedc95ceaae914d4810fa944fedad9d0
zeyyla <anavmittal@berkeley.edu>
2019-11-06 11:18:01 -0800

    Tasks to json

--- alu_server.py
+++ alu_server.py
@@ -3,116 +3,133 @@ import os
 import numpy as np
 from Task import Task, awsTask, SPECIES
 import json
-import pickle
 from sys import exit
 import csv
 import time
 import multiprocessing as mp
 from itertools import product, permutations
 import pandas as pd
+from zipfile import ZipFile
+
 
 
 #TODO: check if local file len matches server file len (tags)
 #TODO: create a json file on the server that stores structure and size information
-def verify_server_data(credentials):
-    td = {}
-    filelist = {file for species_list in [os.listdir("data/" + s) for s in SPECIES] for file in species_list}
-    with mp.Pool(mp.cpu_count() - 1) as p:
-        sizes = p.map(get_len, filelist)
-    for s in SPECIES:
-        td[s] = {"names" : [], "lens" : []}    
-    for s in sizes:
-        species1 = s[0].split("_")[0]
-        td[species1]["names"].append(s[0])
-        td[species1]["lens"].append(s[1])
-    with open("data_structure.json", "w+") as df:
-        json.dump(td, df)
-    
-    s3 = boto3.resource('s3', aws_access_key_id=credentials['aws_access_key_id'], aws_secret_access_key=credentials['aws_secret_access_key'])
-    aludata = s3.Bucket('aludata')
-    filelist = {file for species_list in [os.listdir("data/" + s) for s in SPECIES] for file in species_list}
-    uploaded = {file.key for file in aludata.objects.all()}
-    
-    #remove files that have already been uploaded to the server from filelist
-    filelist.difference_update(uploaded)
+def verify_server_data(params):
 
-    aludata.upload_file("data_structure.json", "data_structure.json")
+    ##new process:
+    '''
+    1) check if zip exists
+    2) check if size of zip matches size of server zip
+    3) if not, reupload
+    '''
+    zipPath = os.path.join(params['datapath'], "data.zip")
+    jsonPath = os.path.join(params['datapath'], "data_structure.json")
 
-    for file in filelist:
-        print("Missing: " + file)
-        path = "data/" + file.split("_")[0] + "/" + file
-        aludata.upload_file(path, file)
-
-    filelist = {file for species_list in [os.listdir("data/" + s) for s in SPECIES] for file in species_list}
-    uploaded = {file.key for file in aludata.objects.all()}
-
-    if filelist == uploaded:
-        print("All server data has been verified")
-    else:
-        print("Error in verifying server data")
+    if not os.path.exists(zipPath):
+        print("Please compress data files into 'data.zip'")
         exit()
+
+    if not os.path.exists(jsonPath):
+        td = {}
+        filelist = {file for species_list in [os.listdir("data/" + s) for s in SPECIES] for file in species_list}
+        with mp.Pool(mp.cpu_count() - 1) as p:
+            sizes = p.map(get_len, filelist)
+        for s in SPECIES:
+            td[s] = {"names" : [], "lens" : []}    
+        for s in sizes:
+            species1 = s[0].split("_")[0]
+            td[species1]["names"].append(s[0])
+            td[species1]["lens"].append(s[1])
+        json.dump(td, open("data_structure.json", "w+"))
+    
+    s3 = boto3.resource('s3', aws_access_key_id=params['aws_access_key_id'], aws_secret_access_key=params['aws_secret_access_key'])
+    aludata = s3.Bucket('aludata')
+    #TODO: add metedata for last_updated, and check if the local file has eben updated since then before uploading
+    aludata.upload_file("data_structure.json", "data_structure.json")
+    print("Server JSON is (already) up-to-date.")
+
+    #check local zipfile size
+    #check server zipfile size
+    #update if mismatch
+    serverZip = aludata.Object("data.zip")
+    # serverZip.
+    with ZipFile(zipPath, "r") as zip_file:
+        localSize = zip_file.file_size
+    if serverSize != localSize:
+        print("Server data does not match local data. Reuploading now.")
+    with tqdm(total=dataFile.content_length, unit='B', unit_scale=True, desc="data.zip") as t:
+        #TODO: chack params
+        aludata.upload_file("data.zip", path.join(dataPath, "data.zip"), Callback=t.update)
+
 
     
 #TODO error handling
-def process_responses(credentials, responses):
+def process_responses(params, responses):
     if "Messages" not in responses.keys():
         return
-    sqs = boto3.client('sqs', aws_access_key_id=credentials['aws_access_key_id'], aws_secret_access_key=credentials['aws_secret_access_key'])
+    sqs = boto3.client('sqs', aws_access_key_id=params['aws_access_key_id'], aws_secret_access_key=params['aws_secret_access_key'])
     for msg in responses['Messages']:
         awstask = awsTask.fromDict(json.loads(msg['Body']))
         print("Recieved task: {} - {} - {} with {} indicies".format(awstask.species1, awstask.species2, awstask.subfamily, len(awstask.indicies)))
-        with open("tasks/" + Task.aws_to_task(awstask), "rb") as taskFile:
-            task = pickle.load(taskFile)
-        with open("results/" + task.filename() + ".csv", "a+", newline='') as csvFile:
-            df = pd.read_csv(csvFile, names = ["ind", "c1", "s1", "e1", "c2", "s2", "e2", "k"])
-            completed = set(df["ind"])
-            datas = [awstask.datas[i] for i in range(len(awstask.datas)) if awstask.datas[i][0] not in completed]
-            wr = csv.writer(csvFile, quoting=csv.QUOTE_ALL)
-            wr.writerows(datas)
+        task = json.load(open("tasks/" + Task.aws_to_task(awstask).replace(".p", ".json"), "r"))
+        task = Task(task["species1"], task["species2"], task["subfamily"], task["total"], task["completed"], task["remaining"], task["candidates"])
+        names = np.concatenate([["ind"]] + [["c"+str(i), "s"+str(i), "e"+str(i)] for i in range(6)])
+        df = pd.read_csv(resultFile, names=names)
+        completed = set(df["ind"])
+        datas = [awstask.datas[i] for i in range(len(awstask.datas)) if awstask.datas[i][0] not in completed]
+        wr = csv.writer(open("results/" + task.filename() + ".csv", "a+", newline=''), quoting=csv.QUOTE_ALL)
+        wr.writerows(datas)
         task.update(awstask)
-        with open("tasks/" + task.filename(), "wb") as taskFile:
-            pickle.dump(task, taskFile)
+        json.dump(task, open("tasks/" + task.filename(), "w"))
         print("Task {} - {} - {} is {}%% done".format(task.species1, task.species2, task.subfamily, task.num_completed()/task.total))
         nextTask = generate_aws_task(awstask)
         if nextTask is not None:
             nextMsg = json.dumps(nextTask.__dict__)
-            response = sqs.send_message(QueueUrl=credentials["task_url"], MessageBody=nextMsg)
+            response = sqs.send_message(QueueUrl=params["task_url"], MessageBody=nextMsg)
             print("Pushed task: {} - {} - {} with {} indicies".format(nextTask.species1, nextTask.species2, nextTask.subfamily, len(nextTask.indicies)))
             print("\tMessage ID: {}".format(response["MessageId"]))
         else:
             print("Task: {} - {} - {} is complete".format(task.species1, task.species2, task.subfamily))
-        sqs.delete_message(QueueUrl=credentials['results_url'], ReceiptHandle=msg['ReceiptHandle'])
+        sqs.delete_message(QueueUrl=params['results_url'], ReceiptHandle=msg['ReceiptHandle'])
 
-def generate_starter_aws_tasks(credentials, size=50):
-    sqs = boto3.client('sqs', aws_access_key_id=credentials['aws_access_key_id'], aws_secret_access_key=credentials['aws_secret_access_key'])
-    attributes = sqs.get_queue_attributes(QueueUrl=credentials['task_url'], AttributeNames=["ApproximateNumberOfMessages"])
+def generate_starter_aws_tasks(params, size=50):
+    sqs = boto3.client('sqs', aws_access_key_id=params['aws_access_key_id'], aws_secret_access_key=params['aws_secret_access_key'])
+    attributes = sqs.get_queue_attributes(QueueUrl=params['task_url'], AttributeNames=["ApproximateNumberOfMessages"])
     approxQueueSize = int(attributes['Attributes']['ApproximateNumberOfMessages'])
     print("We have {} tasks.".format(approxQueueSize))
     if approxQueueSize < 1000:
         print("That's not enough - adding more tasks.")
         for taskFile in os.listdir("tasks/"):
-            t = pickle.load(open("tasks/" + taskFile))
+            if ".json" not in taskFile:
+                continue
+            t = json.load(open("tasks/" + taskFile, "r"))
+            t = Task(t['species1'], t['species2'], t['subfamily'], t['total'], t['completed'], t['remaining'], t['candidates'],)
             awstask = t.get_aws_task(size)
             if awstask is not None:
                 msg = json.dumps(awstask.__dict__)
-                sqs.send_message(QueueUrl=credentials['task_url'], MessageBody=msg)
+                sqs.send_message(QueueUrl=params['task_url'], MessageBody=msg)
         print("Added a bunch of tasks.")        
 
 def generate_aws_task(prevAWSTask):
-    with open("tasks/" + Task.aws_to_task(prevAWSTask), "rb") as taskFile:
-        task = pickle.load(taskFile)
+    task = json.load(open("tasks/" + Task.aws_to_task(prevAWSTask), "r"))
+    task = Task(task["species1"], task["species2"], task["subfamily"], task["total"], task["completed"], task["remaining"], task["candidates"])
     size = len(prevAWSTask.indicies)
     return task.get_aws_task(size)
 
 def get_len(file):
     species1 = file.split("_")[0] 
-    return (file, len(pickle.load(file=open("data/"+species1 + "/" + file, "rb"))))
+    return (file, len(json.load(file=open("data/"+species1 + "/" + file, "r"))))
 
 def verify_task_consistency(taskFile):
-    task = pickle.load(open("tasks/" + taskFile, "rb"))
+    if ".json" not in taskFile:
+        return
+    task = json.load(open("tasks/" + taskFile, "r"))
+    task = Task(task["species1"], task["species2"], task["subfamily"], task["total"], task["completed"], task["remaining"], task["candidates"])
     resultFile = "results/" + task.filename() + ".csv"
     if os.path.exists(resultFile):
-        df = pd.read_csv(resultFile, names=["ind", "c1", "s1", "e1", "c2", "s2", "e2", "k"])
+        names = np.concatenate([["ind"]] + [["c"+str(i), "s"+str(i), "e"+str(i)] for i in range(6)])
+        df = pd.read_csv(resultFile, names=names)
         completed = set(df["ind"])
         if completed != task.completed:
             print("Error with task: {}".format(task.filename()[:-2]))
@@ -121,14 +138,12 @@ def verify_task_consistency(taskFile):
             task.candidates = {i for i in range(task.total)}
             task.remaining.difference_update(completed)
             task.candidates.difference_update(completed)
-            pickle.dump(task, open("tasks/" + taskFile, "wb"))
+            json.dump(task, open("tasks/" + taskFile, "w"))
     else:
         if task.completed != set():
             task.completed = set()
             task.remaining = {i for i in range(task.total)}
-            pickle.dump(task, open("tasks/" + taskFile, "wb"))
-
-
+            json.dump(task, open("tasks/" + taskFile, "w"))
 
 def verify_internal_consistency():
     print("Verifying internal task consistency...")
@@ -136,17 +151,15 @@ def verify_internal_consistency():
         [_ for _ in p.map(verify_task_consistency, os.listdir("tasks/"))]
     print("Task consistency verified.")
 
-
 if __name__ == "__main__":
     credentialFile = "awsServerCredentials.json"
-    with open(credentialFile, "r") as f:
-        credentials = json.load(f)
-    # verify_server_data(credentials)
-    # verify_internal_consistency()
-    generate_starter_aws_tasks(credentials)
-    sqs = boto3.client('sqs', aws_access_key_id=credentials['aws_access_key_id'], aws_secret_access_key=credentials['aws_secret_access_key'])
+    params = json.load(open(credentialFile, "r"))
+    # verify_server_data(params)
+    verify_internal_consistency()
+    generate_starter_aws_tasks(params)
+    sqs = boto3.client('sqs', aws_access_key_id=params['aws_access_key_id'], aws_secret_access_key=params['aws_secret_access_key'])
     while True:
-        attributes = sqs.get_queue_attributes(QueueUrl=credentials['results_url'], AttributeNames=["ApproximateNumberOfMessages"])
+        attributes = sqs.get_queue_attributes(QueueUrl=params['results_url'], AttributeNames=["ApproximateNumberOfMessages"])
         approxQueueSize = int(attributes['Attributes']['ApproximateNumberOfMessages'])
         print("We have {} completed tasks waiting.".format(approxQueueSize))
         if approxQueueSize < 100:
@@ -155,6 +168,6 @@ if __name__ == "__main__":
             time.sleep(30)
         else:
             for _ in range(approxQueueSize//10):
-                response = sqs.receive_message(QueueUrl=credentials["results_url"], MaxNumberOfMessages=10, WaitTimeSeconds=20)
-                process_responses(credentials, response)
+                response = sqs.receive_message(QueueUrl=params["results_url"], MaxNumberOfMessages=10, WaitTimeSeconds=20)
+                process_responses(params, response)
     
